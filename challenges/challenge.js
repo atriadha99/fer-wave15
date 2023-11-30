4bded504fa83dfcbf05824f93e2afa26a452dfd2
atriadha99 <atriadha@gmail.com>
2023-11-30 21:23:38 +0700

    new file and folder

---
+++ challenges/challenge.js
@@ -0,0 +1,407 @@
+// Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000
+color: black,
+ font-size: 14px,
+ font-family: Arial,
+ font-weight: 700,
+ line-height: 20px,
+ word-wrap: break-word,
+
+// binarcarrental@gmail.com
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// 081-233-334-808
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Our services
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 400;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Why Us
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 400;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Testimonial
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 400;
+ line-height: 20px;
+ word-wrap: break-word
+
+// FAQ
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 400;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Connect with us
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Copyright Binar 2022
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Frequently Asked Question
+color: black;
+ font-size: 24px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 36px;
+ word-wrap: break-word
+
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Apa saja syarat yang dibutuhkan?
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Berapa hari minimal sewa mobil lepas kunci?
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Berapa hari sebelumnya sabaiknya booking sewa mobil?
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Apakah Ada biaya antar-jemput?
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Bagaimana jika terjadi kecelakaan
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Sewa Mobil di (Lokasimu) Sekarang
+color: white;
+ font-size: 36px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 54px;
+ word-wrap: break-word
+
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+color: white;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+---
+// Mulai Sewa Mobil
+color: white;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+---
+// Testimonial
+color: black;
+ font-size: 24px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 36px;
+ word-wrap: break-word
+---
+// Berbagai review positif dari para pelanggan kami
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+---
+// “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod”
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+---
+// John Dee 32, Bromo
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 400;
+ line-height: 20px;
+ word-wrap: break-word
+---
+// “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod”
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// John Dee 32, Bromo
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 400;
+ line-height: 20px;
+ word-wrap: break-word
+
+// “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod”
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// John Dee 32, Bromo
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 400;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Why Us?
+color: black;
+ font-size: 24px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 36px;
+ word-wrap: break-word
+
+// Mengapa harus pilih Binar Car Rental?
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Mobil Lengkap
+color: black;
+ font-size: 16px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 24px;
+ word-wrap: break-word
+
+// Tersedia banyak pilihan mobil, kondisi masih baru, bersih dan terawat
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Harga Murah
+color: black;
+ font-size: 16px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 24px;
+ word-wrap: break-word
+
+// Harga murah dan bersaing, bisa bandingkan harga kami dengan rental mobil lain
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Layanan 24 Jam
+color: black;
+ font-size: 16px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 24px;
+ word-wrap: break-word
+
+// Siap melayani kebutuhan Anda selama 24 jam nonstop. Kami juga tersedia di akhir minggu
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Sopir Profesional
+color: black;
+ font-size: 16px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 24px;
+ word-wrap: break-word
+
+// Sopir yang profesional, berpengalaman, jujur, ramah dan selalu tepat waktu
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Best Car Rental for any kind of trip in (Lokasimu)!
+color: black;
+ font-size: 24px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 36px;
+ word-wrap: break-word
+
+// Sewa mobil di (Lokasimu) bersama Binar Car Rental jaminan harga lebih murah dibandingkan yang lain, kondisi mobil baru, serta kualitas pelayanan terbaik untuk perjalanan wisata, bisnis, wedding, meeting, dll.
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Sewa Mobil Dengan Supir di Bali 12 Jam
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Sewa Mobil Lepas Kunci di Bali 24 Jam
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Sewa Mobil Jangka Panjang Bulanan
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Gratis Antar - Jemput Mobil di Bandara
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Layanan Airport Transfer / Drop In Out
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)
+color: black;
+ font-size: 36px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 54px;
+ word-wrap: break-word
+
+// Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Mulai Sewa Mobil
+color: white;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 700;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Our Services
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 400;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Why Us
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 400;
+ line-height: 20px;
+ word-wrap: break-word
+
+// Testimonial
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 400;
+ line-height: 20px;
+ word-wrap: break-word
+
+// FAQ
+color: black;
+ font-size: 14px;
+ font-family: Arial;
+ font-weight: 400;
+ line-height: 20px;
+ word-wrap: break-word
